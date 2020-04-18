b1b6330daaf7010218284253525ebe2190c8c2c3
TomekkM <o2.tomek@tlen.pl>
2020-04-18 09:49:46 +0200

    add fake data service

---
+++ EasyMoney/EasyMoney.Fake/FakeService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EasyMoney.Fake
+{
+    public class FakeService : IFakeService
+    {
+        private List<string> FakeData = new List<string> { "first", "second", "third" };
+
+        public IEnumerable<string> GetAll()
+        {
+            return FakeData;
+        }
+
+        public string GetFirst()
+        {
+            return FakeData.First();
+        }
+
+        public void Add(string value)
+        {
+            FakeData.Add(value);
+        }
+    }
+}
---
+++ EasyMoney/EasyMoney.Fake/IFakeService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace EasyMoney.Fake
+{
+    public interface IFakeService
+    {
+        string GetFirst();
+        IEnumerable<string> GetAll();
+        void Add(string value);
+    }
+}

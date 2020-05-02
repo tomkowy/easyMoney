3f4376fd14805eacdc3caacd9c488f5289500f69
TomekkM <o2.tomek@tlen.pl>
2020-05-02 15:39:02 +0200

    add fake test

---
+++ EasyMoney/EasyMoney.Tests.FakeTest/FakeTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace EasyMoney.Tests.FakeTest
+{
+    public class FakeTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}

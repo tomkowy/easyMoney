acdd5ea9cfa6f467e8a71f8b190aefaa7a8a62e9
TomekkM <o2.tomek@tlen.pl>
2020-05-09 12:55:13 +0200

    add user entity

---
+++ EasyMoney/EasyMoney.Modules.FakeManageUsers.Domain/Users/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EasyMoney.Modules.FakeManageUsers.Domain.Users
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Email { get; private set; }
+        public bool Active { get; private set; }
+
+        private User() { }
+
+        private User(string email)
+        {
+            Email = email;
+        }
+
+        internal static User Create(string email)
+        {
+            return new User(email);
+        }
+
+        internal void Activate()
+        {
+            Active = true;
+        }
+    }
+}

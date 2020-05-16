438ef2da5b3704632d916e871769fc4a79633da4
TomekkM <o2.tomek@tlen.pl>
2020-05-16 10:41:05 +0200

    get sent emails count

---
+++ EasyMoney/EasyMoney.Modules.FakeNotifications.Application/GetSentEmailsCount/GetSentEmailsCountQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace EasyMoney.Modules.FakeNotifications.Application.GetSentEmailsCount
+{
+    public class GetSentEmailsCountQuery : IRequest<int>
+    {
+
+    }
+}
---
+++ EasyMoney/EasyMoney.Modules.FakeNotifications.Application/GetSentEmailsCount/GetSentEmailsCountQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using EasyMoney.Modules.FakeNotifications.Infrastructure.Context;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasyMoney.Modules.FakeNotifications.Application.GetSentEmailsCount
+{
+    internal class GetSentEmailsCountQueryHandler : IRequestHandler<GetSentEmailsCountQuery, int>
+    {
+        private readonly NotificationsContext _context;
+
+        public GetSentEmailsCountQueryHandler(NotificationsContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(GetSentEmailsCountQuery request, CancellationToken cancellationToken)
+        {
+            var count = await _context.Emails.CountAsync();
+            return count;
+        }
+    }
+}

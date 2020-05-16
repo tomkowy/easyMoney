657465b61354146483ffaf546f37308bb7e044a5
TomekkM <o2.tomek@tlen.pl>
2020-05-16 08:06:45 +0200

    add notifications domain project

---
+++ EasyMoney/EasyMoney.Modules.Notifications.Domain/Emails/Email.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasyMoney.Modules.FakeNotifications.Domain.Emails
+{
+    public class Email
+    {
+        public Guid Id { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string Addresses { get; set; }
+    }
+}
